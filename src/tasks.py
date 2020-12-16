f4d77cee520c36ff663bd91225825e6b5b507f1a
Carlos Catalan <carloscatalan94c@gmail.com>
2020-12-16 14:04:28 -0700

    Eit cache

--- src/tasks.py
+++ src/tasks.py
@@ -2,6 +2,5 @@ tasks = [
     {'name': 'Integrate', 'description': 'CI...'},
     {'name': 'Delivery', 'description': 'CD...'},
     {'name': 'Deployment', 'description': 'CD...'},
-    {'name': 'New Feature', 'description': 'Perfect!'},
     {'name': 'New Feature on main branch', 'description': 'Works!'}
 ]
