c9afe259c426335d90a9e25d021d3f2040a765ac
Carlos Catalan <carloscatalan94c@gmail.com>
2020-12-16 10:09:50 -0700

    SyntaxError

--- src/tasks.py
+++ src/tasks.py
@@ -2,6 +2,6 @@ tasks = [
     {'name': 'Integrate', 'description': 'CI...'},
     {'name': 'Delivery', 'description': 'CD...'},
     {'name': 'Deployment', 'description': 'CD...'},
-    {'name': 'New Feature', 'description': 'Perfect!'}
+    {'name': 'New Feature', 'description': 'Perfect!'},
     {'name': 'New Feature on main branch', 'description': 'Works!'}
 ]
