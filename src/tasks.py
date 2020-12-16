c13a13eda6f25c1490855d4c09301e2987f349be
Carlos Catalan <carloscatalan94c@gmail.com>
2020-12-16 15:16:36 -0700

    Edit Systemctl

--- src/tasks.py
+++ src/tasks.py
@@ -2,5 +2,5 @@ tasks = [
     {'name': 'Integrate', 'description': 'CI...'},
     {'name': 'Delivery', 'description': 'CD...'},
     {'name': 'Deployment', 'description': 'CD...'},
-    {'name': 'New Feature on main branch', 'description': 'Works!'}
+    {'name': 'New Feature on main branch', 'description': 'This works!'}
 ]
