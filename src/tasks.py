3f728aa562cc762c1d530436d176136dfe3ba40a
Carlos Catalan <carloscatalan94c@gmail.com>
2020-12-16 10:04:12 -0700

    aws ready

--- src/tasks.py
+++ src/tasks.py
@@ -3,4 +3,5 @@ tasks = [
     {'name': 'Delivery', 'description': 'CD...'},
     {'name': 'Deployment', 'description': 'CD...'},
     {'name': 'New Feature', 'description': 'Perfect!'}
+    {'name': 'New Feature on main branch', 'description': 'Works!'}
 ]
