ff26fa77acdbf84111db2aa5008e1b258cf035c7
Carlos Catalan <carloscatalan94c@gmail.com>
2020-12-17 16:53:47 -0700

    new ùsh

--- src/tasks.py
+++ src/tasks.py
@@ -2,5 +2,5 @@ tasks = [
     {'name': 'Integrate', 'description': 'CI...'},
     {'name': 'Delivery', 'description': 'CD...'},
     {'name': 'Deployment', 'description': 'CD...'},
-    {'name': 'New Feature on main branch X 2', 'description': 'This works!'}
+    {'name': 'New Featuree on main branch X 2', 'description': 'This works!'}
 ]
