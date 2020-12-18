3a058bc0da9ab4c65f96f46fdbb37a513a5fabe9
Carlos Catalan <carloscatalan94c@gmail.com>
2020-12-18 15:43:21 -0700

    docker pull

--- src/tasks.py
+++ src/tasks.py
@@ -2,5 +2,5 @@ tasks = [
     {'name': 'Integrate', 'description': 'CI...'},
     {'name': 'Delivery', 'description': 'CD...'},
     {'name': 'Deployment', 'description': 'CD...'},
-    {'name': 'New Featuree on main branch X 2', 'description': 'This works!'}
+    {'name': 'New Feature', 'description': 'Using Docker Pull!!!'}
 ]
