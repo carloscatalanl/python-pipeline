6fcf659174d44a01a3d4d1ddc2da7b9b82db1081
Carlos Catalan <carloscatalan94c@gmail.com>
2020-12-18 16:13:54 -0700

    Pipeline Ready

--- src/tasks.py
+++ src/tasks.py
@@ -2,5 +2,6 @@ tasks = [
     {'name': 'Integrate', 'description': 'CI...'},
     {'name': 'Delivery', 'description': 'CD...'},
     {'name': 'Deployment', 'description': 'CD...'},
-    {'name': 'New Feature', 'description': 'SSH'}
+    {'name': 'New Feature', 'description': 'SSH'},
+    {'name': 'Ready', 'description': 'Pipeline'}
 ]
