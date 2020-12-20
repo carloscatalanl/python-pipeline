625ac4388881d9af64f41d63070dc5f3ca57422f
Carlos Catalan <carloscatalan94c@gmail.com>
2020-12-20 13:04:32 -0700

    Task

--- src/tasks.py
+++ src/tasks.py
@@ -2,6 +2,5 @@ tasks = [
     {'name': 'Integrate', 'description': 'CI...'},
     {'name': 'Delivery', 'description': 'CD...'},
     {'name': 'Deployment', 'description': 'CD...'},
-    {'name': 'New Feature', 'description': 'SSH'},
-    {'name': 'Ready', 'description': 'Pipeline'}
+    {'name': 'New Feature', 'description': 'SSH'}
 ]
