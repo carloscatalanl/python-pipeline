2030b666e23bf2e524d9ab12a35e6b445338d3ec
Carlos Catalan <carloscatalan94c@gmail.com>
2020-12-19 20:36:37 -0700

    User data

--- tf/modules/instances/ec2.tf
+++ tf/modules/instances/ec2.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "ec2-public" {
   vpc_security_group_ids = [aws_security_group.public_sg.id]
   associate_public_ip_address = true
   key_name = aws_key_pair.key_access.key_name
+  user_data = file("${path.module}/src/start.sh")
 
   lifecycle {
     create_before_destroy = true
---
+++ tf/modules/instances/src/start.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+## Install Docker 
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo apt-get install -y docker-compose
+
+
+cd /home/ubuntu
+mkdir app
+
+
+## Create docker-file
+echo "version: '3.3'
+
+services:
+  python-api:
+    image: catalan94/python-pipeline:latest
+    ports:
+      - "80:4000"
+    restart: always
+" >> ./app/docker-compose.yml
+
+
+## Create docker boot service
+echo "[Unit]
+Description=docker boot
+After=docker.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+WorkingDirectory=/home/ubuntu/app
+ExecStartPre=sudo docker-compose -f /home/ubuntu/app/docker-compose.yml pull
+ExecStart=sudo docker-compose -f /home/ubuntu/app/docker-compose.yml up -d
+
+[Install]
+WantedBy=multi-user.target
+" >> /etc/systemd/system/docker_boot.service
+
+
+## Enable and start docker boot service
+sudo systemctl enable docker_boot.service
+sudo systemctl start docker_boot.service
+
+
+sudo docker-compose -f /home/ubuntu/app/docker-compose.yml up -d
